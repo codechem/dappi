4f16882a5dee28432c6676163b38293821ce8553
fvanderflier <fvanderflier@halicea.com>
2025-05-12 16:48:14 +0200

    change project references to nuget package usage during init

--- Dappi.Cli/StringExtensions.cs
+++ Dappi.Cli/StringExtensions.cs
@@ -4,77 +4,79 @@ namespace Dappi.Cli;
 
 public static class StringExtensions
 {
-        public static (string userName, string repoName, string tagName) TemplateNameParse(this string templateName)
+    public static (string userName, string repoName, string tagName) TemplateNameParse(this string templateName)
+    {
+        if (templateName.IsNullOrWhiteSpace())
         {
-            if (templateName.IsNullOrWhiteSpace())
-            {
-                throw new ArgumentNullException(nameof(templateName));
-            }
-
-            if (templateName.IndexOf('/') <= 0 || templateName.EndsWith('/'))
-            {
-                Console.WriteLine("Invalid TemplateName!Please Enter As Format aspnetboilerplate/module-zero-core-template@v4.2.0 ");
-                throw new ArgumentException("Invalid TemplateName", nameof(templateName));
-            }
-
-            var tNames = templateName.Split(new char[] { '/', '@' });
-            var userName = string.Empty;
-            var repoName = string.Empty;
-            var tagName = "1.0.0";
-            if (tNames.Length > 1)
-            {
-                userName = tNames[0];
-                repoName = tNames[1];
-            }
-
-            if (tNames.Length > 2)
-            {
-                tagName = tNames[2];
-            }
-
-            return (userName, repoName, tagName);
+            throw new ArgumentNullException(nameof(templateName));
         }
 
-        public static bool IsNullOrWhiteSpace(this string v)
+        if (templateName.IndexOf('/') <= 0 || templateName.EndsWith('/'))
         {
-            return string.IsNullOrWhiteSpace(v);
+            Console.WriteLine(
+                "Invalid TemplateName!Please Enter As Format aspnetboilerplate/module-zero-core-template@v4.2.0 ");
+            throw new ArgumentException("Invalid TemplateName", nameof(templateName));
         }
 
-        public static (string company, string project, string module) NameParse(this string name)
+        var tNames = templateName.Split(new char[] { '/', '@' });
+        var userName = string.Empty;
+        var repoName = string.Empty;
+        var tagName = "1.0.0";
+        if (tNames.Length > 1)
         {
-            if (name.IsNullOrWhiteSpace())
-            {
-                throw new ArgumentNullException(nameof(name));
-            }
-
-            if (name.IndexOf('.') <= 0)
-            {
-                return (string.Empty, name, string.Empty);
-            }
-            
-            if (name.EndsWith('.'))
-            {
-                Console.WriteLine("Invalid Format! Please Enter As Format: CompanyName.ProjectName or CompanyName.ProjectName.ModuleName!");
-                throw new ArgumentException("Invalid Format", nameof(name));
-            }
-
-            var tNames = name.Split('.');
-            var company = string.Empty;
-            var project = string.Empty;
-            var module = string.Empty;
-            if (tNames.Length > 1)
-            {
-                company = tNames[0];
-                project = tNames[1];
-            }
-
-            if (tNames.Length > 2)
-            {
-                module = tNames[2];
-            }
-
-            return (company, project, module);
+            userName = tNames[0];
+            repoName = tNames[1];
         }
+
+        if (tNames.Length > 2)
+        {
+            tagName = tNames[2];
+        }
+
+        return (userName, repoName, tagName);
+    }
+
+    public static bool IsNullOrWhiteSpace(this string v)
+    {
+        return string.IsNullOrWhiteSpace(v);
+    }
+
+    public static (string company, string project, string module) NameParse(this string name)
+    {
+        if (name.IsNullOrWhiteSpace())
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+
+        if (name.IndexOf('.') <= 0)
+        {
+            return (string.Empty, name, string.Empty);
+        }
+
+        if (name.EndsWith('.'))
+        {
+            Console.WriteLine(
+                "Invalid Format! Please Enter As Format: CompanyName.ProjectName or CompanyName.ProjectName.ModuleName!");
+            throw new ArgumentException("Invalid Format", nameof(name));
+        }
+
+        var tNames = name.Split('.');
+        var company = string.Empty;
+        var project = string.Empty;
+        var module = string.Empty;
+        if (tNames.Length > 1)
+        {
+            company = tNames[0];
+            project = tNames[1];
+        }
+
+        if (tNames.Length > 2)
+        {
+            module = tNames[2];
+        }
+
+        return (company, project, module);
+    }
 
 
     public static string EnsureEndsWith(this string v, char b)
