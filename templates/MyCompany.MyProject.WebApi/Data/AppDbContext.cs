2519c5728eda51a002392ac718fbfe17f9c0667b
manevdushko <dushko@halicea.com>
2025-05-13 11:48:59 +0200

    reset templates folder

--- templates/MyCompany.MyProject.WebApi/Data/AppDbContext.cs
+++ templates/MyCompany.MyProject.WebApi/Data/AppDbContext.cs
@@ -1,4 +1,3 @@
-using MyCompany.MyProject.WebApi.Entities;
 using CCApi.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -9,18 +8,4 @@ public class AppDbContext : IdentityDbContext<DappiUser, DappiRole, string>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
-
-	public DbSet<testCollection> testCollections { get; set; }
-
-	public DbSet<test1> test1s { get; set; }
-
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<test1>()
-            .HasMany(m => m.testCollections)
-            .WithMany(r => r.test1s)
-            .UsingEntity(j => j.ToTable("test1testCollections"));
-
-        base.OnModelCreating(modelBuilder);
-    }}
+}
