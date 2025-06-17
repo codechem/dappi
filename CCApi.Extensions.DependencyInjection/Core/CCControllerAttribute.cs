8a9fd413b81c2630f8aedd3dfc56b099b9439bdb
manevdushko <dushko@halicea.com>
2025-06-17 17:18:53 +0200

    fix: Clean the port so the application can be started (#82)
    * clean the port
    
    * small refactor
    
    ---------
    
    Co-authored-by: fvanderflier <fvanderflier@halicea.com>

--- CCApi.Extensions.DependencyInjection/Core/CCControllerAttribute.cs
+++ CCApi.Extensions.DependencyInjection/Core/CCControllerAttribute.cs
@@ -1,7 +1,7 @@
 // TODO: We should probably move this namespace as a separate project shared between SourceGenerator and HeadlessCms
 namespace CCApi.Extensions.DependencyInjection.Core
 {
-      
+
     [AttributeUsage(AttributeTargets.Class)]
     public class CCControllerAttribute : Attribute
     {
