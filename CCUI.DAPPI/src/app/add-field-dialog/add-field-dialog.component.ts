27dd0af8c90ddf97730ce1945fe8fc4a815ce853
gorgi-lazarev <gorgi.lazarev@halicea.com>
2025-08-18 16:13:35 +0200

    fix: Pluralization of properties and tables (#110)
    * Add pluralization of model names
    
    * [FIX] #108 | Include the Pluralize package in the built project
    
    ---------
    
    Co-authored-by: Gorgi Lazarev <gorgi.lazarev@codechem.com>

--- CCUI.DAPPI/src/app/add-field-dialog/add-field-dialog.component.ts
+++ CCUI.DAPPI/src/app/add-field-dialog/add-field-dialog.component.ts
@@ -20,6 +20,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { selectCollectionTypes, selectFields } from '../state/collection/collection.selectors';
 import { selectSelectedType } from '../state/content/content.selectors';
 import { Subscription } from 'rxjs';
+import pluralize from 'pluralize';
 import { EnumsResponse } from '../models/enums-response.model';
 import { EnumsService } from '../services/common/enums.service';
 import { ModelValidators } from '../validators/model-validators';
@@ -223,17 +224,17 @@ export class AddFieldDialogComponent implements OnInit, OnDestroy {
     this.relationTypes = [
       {
         label: 'Many-to-many',
-        description: `Many ${this.selectedType}s can relate to many ${modelName}s`,
+        description: `Many ${pluralize(this.selectedType)} can relate to many ${pluralize(modelName)}`,
         value: 'many-to-many',
       },
       {
         label: 'One-to-many',
-        description: `One ${this.selectedType} can relate to many ${modelName}s`,
+        description: `One ${this.selectedType} can relate to many ${pluralize(modelName)}`,
         value: 'one-to-many',
       },
       {
         label: 'Many-to-one',
-        description: `Many ${this.selectedType}s can relate to one ${modelName}`,
+        description: `Many ${pluralize(this.selectedType)} can relate to one ${modelName}`,
         value: 'many-to-one',
       },
       {
