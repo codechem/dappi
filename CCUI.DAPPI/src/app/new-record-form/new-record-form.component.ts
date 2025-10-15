72e2042e0907e4649287bb633445ebb345345c70
David Pavlovski <davidpavlovski@Mac-8.home>
2025-10-15 13:07:39 +0200

    fix input for dateonly fields

--- CCUI.DAPPI/src/app/new-record-form/new-record-form.component.ts
+++ CCUI.DAPPI/src/app/new-record-form/new-record-form.component.ts
@@ -1,5 +1,5 @@
-import { Location } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Location , formatDate} from '@angular/common';
+import { Component, Inject, LOCALE_ID, OnDestroy, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -103,7 +103,8 @@ export class NewRecordFormComponent implements OnInit, OnDestroy {
     private fb: FormBuilder,
     private location: Location,
     private store: Store,
-    private actions$: Actions
+    private actions$: Actions,
+    @Inject(LOCALE_ID)private locale: string
   ) {
     this.contentForm = this.fb.group({});
   }
@@ -248,9 +249,11 @@ export class NewRecordFormComponent implements OnInit, OnDestroy {
     this.subscriptions.add(
       this.headers$.subscribe((fields) => {
         this.fields = fields;
+        console.log(FieldType);
         this.contentFields = this.fields
           .filter((field) => field.type !== FieldType.id)
           .map((field) => {
+            console.log(field);
             const contentField: ContentField = {
               key: field.key,
               label: field.label,
@@ -582,7 +585,9 @@ export class NewRecordFormComponent implements OnInit, OnDestroy {
           } else {
             formData[key] = value;
           }
-        } else {
+        } else if(field?.type === FieldType.dateonly && value){
+          formData[key] = formatDate(value, "yyyy-MM-dd" , this.locale);
+        }else {
           formData[key] = value;
         }
       });
