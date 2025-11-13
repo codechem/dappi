5ab2626a9d52d7af0542440c3bf1ca1ccec636de
David Pavlovski <davidpavlovski@Davids-MacBook-Air.local>
2025-11-13 10:08:05 +0100

    change test environment

--- Dappi.TestEnv/Program.cs
+++ Dappi.TestEnv/Program.cs
@@ -1,28 +1,24 @@
-﻿using Dappi.HeadlessCms;
-using Dappi.HeadlessCms.Database;
+using Dappi.HeadlessCms;
 using Dappi.HeadlessCms.Models;
 using Dappi.TestEnv.Data;
-using Microsoft.AspNetCore.Builder;
 
-namespace Dappi.TestEnv
+namespace Dappi.TestEnv;
+public class Program
 {
-    public class Program
+    private static async Task Main(string[] args)
     {
-        private static async Task Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddDappi<TestDbContext>(builder.Configuration);
-            builder.Services.AddDappiAuthentication<DappiUser, DappiRole, TestDbContext>(builder.Configuration);
+        builder.Services.AddDappi<TestDbContext>(builder.Configuration);
+        builder.Services.AddDappiAuthentication<DappiUser, DappiRole, TestDbContext>(builder.Configuration);
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            await app.UseDappi<TestDbContext>();
+        await app.UseDappi<TestDbContext>();
         
-            app.UseHttpsRedirection();
-            app.MapControllers();
+        app.UseHttpsRedirection();
+        app.MapControllers();
         
-            app.Run();
-        }
+        app.Run();
     }
 }
